675e88e7d3a831dc38d403acc9789f91b246d440
claytoncook12 <claytoncook12@gmail.com>
2021-12-06 17:07:12 -0500

    add STATIC_ROOT setting

--- DjangoApp/core/settings.py
+++ DjangoApp/core/settings.py
@@ -140,6 +140,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = Path(BASE_DIR) / 'static'
+STATICFILES = [STATIC_ROOT]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
